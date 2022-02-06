bfd3edf3d5dfa4a65b18f58e5f1a196792f4bc46
Marculonis21 <markbecvar@gmail.com>
2022-02-06 11:33:02 +0100

    before ray change

--- Raymagic/Raymagic/RayMarchingHelper.cs
+++ Raymagic/Raymagic/RayMarchingHelper.cs
@@ -3,6 +3,18 @@ using Microsoft.Xna.Framework;
 
 namespace Raymagic
 {
+    public struct Ray
+    {
+        public Vector3 origin {get; private set;}
+        public Vector3 direction {get; private set;}
+
+        public Ray(Vector3 origin, Vector3 direction)
+        {
+            this.origin = origin;
+            this.direction = Vector3.Normalize(direction);
+        }
+    }
+
     public static class RayMarchingHelper
     {
         public static void RayMarch(Vector3 position, Vector3 dir, out float length, out Color color)
