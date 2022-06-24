eb214ecf833c8403e1581c4ea5a79bd6c7e23cbc
Marculonis21 <markbecvar@gmail.com>
2022-06-24 20:48:21 +0200

    Extension methods change - new Vector<double> -> Vector3

--- Raymagic/Raymagic/ExtensionMethods/ExtensionMethods.cs
+++ Raymagic/Raymagic/ExtensionMethods/ExtensionMethods.cs
@@ -2,10 +2,11 @@ using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
+using Extreme.Mathematics;
 
 namespace Raymagic
 {
-    public static class Vector3Extensions
+    public static class ExtensionMethods
     {
         public static Color ToColor(this Vector3 v)
         {
@@ -13,6 +14,11 @@ namespace Raymagic
             int g = (int)(v.Y * 255);
             int b = (int)(v.Z * 255);
             return new Color(r,g,b);
+        }
+
+        public static Vector3 ToVector3(this Vector<double> v)
+        {
+            return new Vector3((float)v[0], (float)v[1], (float)v[2]);
         }
     }
 }
